853368c14e48bf52ff5fab0781711d13de29d827
chenqi <chen_q_i@163.com>
2017-07-06 14:12:47 +0800

    整理

---
+++ js/mytest/jqchajian.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/5/19 0019.
+ */
+
+
+$.extend({
+    sayHello: function(name) {
+        console.log('Hello,' + (name ? name : 'Dude') + '!');
+    }
+})
+$.sayHello(); //调用
+$.sayHello('Wayou'); //带参调用
